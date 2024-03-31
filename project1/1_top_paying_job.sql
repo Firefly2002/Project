0658112e6957463abb083453581d87b40a5ca235
flamcoder <152239446+Firefly2002@users.noreply.github.com>
2024-03-31 14:49:57 +1100

    upload sql file

---
+++ project1/1_top_paying_job.sql
@@ -0,0 +1,37 @@
+/*
+Question: What are the top_paying data analyst jobs?
+-Identify the top 10 highest paying Data Analyst roles that are avaible remotely.
+- Focuses on job postings with specified salaries.
+- Why? Highlight the top_paying opportunities for Data Analysts.
+*/
+
+with top_paying_jobs AS (
+    SELECT
+        job_id,
+        job_title,
+        job_location,
+        job_schedule_type,
+        salary_year_avg,
+        job_posted_date,
+        name as company_name
+
+    FROM
+        job_postings_fact
+    LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+    WHERE
+        job_title_short = 'Data Analyst' AND
+        job_location = 'Anywhere' AND
+        salary_year_avg is NOT NULL
+    ORDER BY
+        salary_year_avg desc
+    LIMIT 10
+)
+
+SELECT 
+    top_paying_jobs.*,
+    skills
+FROM top_paying_jobs
+INNER JOIN skills_job_dim on top_paying_jobs.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim on skills_job_dim.skill_id = skills_dim.skill_id
+ORDER BY
+    salary_year_avg DESC
